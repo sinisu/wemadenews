935ece945a6d93fead47f7f98ed2f29444f9c191
yisoo song <yisoo1049@gmail.com>
2024-02-20 21:08:14 +0900

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY=`1ca59444f6184e01a70725c06a31ae22`
 let news=[];
 const getLatestNews = async ()=>{
-    const url= new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    const url= new URL(`https://wemadenews.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
     const response = await fetch(url) //fetch는 url로 정보를 요청하는것
     const data = await response.json();
     news = data.articles;
